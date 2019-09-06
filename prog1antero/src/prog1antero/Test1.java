2b294e1795215a7c2e4a24ca8ec97daed8c5c3fd
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-09-06 10:09:25 +0200

    start

---
+++ prog1antero/src/prog1antero/Test1.java
@@ -0,0 +1,32 @@
+package prog1antero;
+
+public class Test1 {
+	public static void main(String [] arg) {
+		int y; //deklarerar variabeln y
+		
+		y = 1; //tilldelar 1 till variabeln y
+		
+		int x = 1; // deklarering + tilldelning = initsiering
+		
+		double tal3 = 3.0;
+		
+		String mening = "hej jag heter"; //
+				
+		char ettTecken = 'a';
+		
+		boolean logik = true;
+		
+		if (logik == true )
+			20
+			10
+			6
+			
+	
+		
+		
+		
+		
+		System.out.print(ettTecken + mening); 
+	}
+
+}
