4e9950db1b2a40c590fce09d3745e910ea6e9058
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-09-06 10:49:05 +0200

    Exempel

---
+++ prog1antero/src/prog1antero/Exempel02_02.java
@@ -0,0 +1,21 @@
+package prog1antero;
+
+import java.util.Scanner;
+
+public class Exempel02_02 {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		
+		System.out.print("Ange ditt namn :");
+		
+		String namn = input.nextLine();
+		
+		System.out.println(namn);
+	}
+	
+	
+	
+	
+}
