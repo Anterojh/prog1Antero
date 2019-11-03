6912a827f6fc03988f1de7a8c00b43383dff5d69
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-11-03 12:28:36 +0100

    öv

---
+++ prog1antero/src/prog1antero/öv22.java
@@ -0,0 +1,25 @@
+package prog1antero;
+
+import java.util.Scanner;
+
+public class öv22 {
+
+	public static void main(String[] args) {
+		
+	Scanner input=new Scanner(System.in);
+		
+		System.out.print("Skriv ett ord: ");
+		
+		String ord = input.nextLine(); 
+		
+		if(ord == "vatten") {
+			
+	     System.out.print("Vatten är water på engelska");
+	     
+		}
+	     
+			}
+		
+		
+
+}
