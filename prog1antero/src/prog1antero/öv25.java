ef72f56106cb155be2d37b414fee123da7167145
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-11-03 12:51:45 +0100

    öv25

---
+++ prog1antero/src/prog1antero/öv25.java
@@ -0,0 +1,31 @@
+package prog1antero;
+
+import java.util.Scanner;
+
+public class öv25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Ange ett tal: ");
+		
+		double a = input.nextDouble();
+		
+		double b = a+0.5;
+		
+		int c = (int) b;
+		
+		System.out.print(c);
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
