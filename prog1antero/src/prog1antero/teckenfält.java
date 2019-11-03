68668bb936e307950533ad2dcb4e3d34274ff67c
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-11-03 12:07:18 +0100

    övning

---
+++ prog1antero/src/prog1antero/teckenfält.java
@@ -0,0 +1,13 @@
+package prog1antero;
+
+public class teckenfält {
+	public static void main(String[] Args) {
+		char[] namn = new char[2];
+		
+		 char[] namn = {'P','E'};
+		
+		System.out.print(namn[0] + namn[1]);
+		
+	}
+
+}
