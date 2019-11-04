fc20443398150e0d0f163c9b21887fafb656da62
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-11-04 11:25:30 +0100

    öv45

---
+++ prog1antero/src/prog1antero/öv45.java
@@ -0,0 +1,21 @@
+package prog1antero;
+
+public class öv45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5 + 6%4;
+		int b = (5 + 6)%4;
+		double c = 5;
+		c *= 2.5 + 1;
+		
+	System.out.println("a = " + a);
+	System.out.println("b = " + b);
+	System.out.println("c = " + c);
+	
+	
+		
+	}
+
+}
