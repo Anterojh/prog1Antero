44baccd160d7e3ccd38ae84f2a0d0d2a863e9664
anthjo1 <anthjo1@5CD7172DXC.thn.local>
2019-12-02 11:41:41 +0100

    öv61

---
+++ prog1antero/src/prog1antero/öv61.java
@@ -0,0 +1,19 @@
+package prog1antero;
+
+import java.util.Scanner;
+
+public class öv61 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("Ange 3 heltal: ");
+int tal1=input.nextInt() *2;
+int tal2=input.nextInt() *3;
+int tal3=input.nextInt() *4;
+
+int svar = tal1+tal2+tal3;
+System.out.println(svar);
+	}
+
+}
